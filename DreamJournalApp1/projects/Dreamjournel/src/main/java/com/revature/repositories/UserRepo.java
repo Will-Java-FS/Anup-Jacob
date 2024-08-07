049f974a4f7438feaa796a8a4c57c2e3b7372e62
Anup-jacob759 <coldsteelrean30@gmail.com>
2024-08-07 11:36:40 -0400

    Delete DreamJournalApp1 directory

--- DreamJournalApp1/projects/Dreamjournel/src/main/java/com/revature/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User,Integer> {
-}
